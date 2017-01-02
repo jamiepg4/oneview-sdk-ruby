c20e10e04c99ecf12ac56613a31bcbe7fe8e1abe
Ricardo Galeno <ricardogaleno.rg@gmail.com>
2017-01-02 20:40:25 +0000

    Fixed uplink set tests

--- spec/integration/resource/api300/c7000/uplink_set/create_spec.rb
+++ spec/integration/resource/api300/c7000/uplink_set/create_spec.rb
@@ -4,25 +4,13 @@ klass = OneviewSDK::API300::C7000::UplinkSet
 RSpec.describe klass, integration: true, type: CREATE, sequence: seq(klass) do
   include_context 'integration api300 context'
 
-  let(:uplink_data) do
-    {
-      nativeNetworkUri: nil,
-      reachability: 'Reachable',
-      manualLoginRedistributionState: 'NotSupported',
-      connectionMode: 'Auto',
-      lacpTimer: 'Short',
-      networkType: 'Ethernet',
-      ethernetNetworkType: 'Tagged',
-      description: nil,
-      name: UPLINK_SET4_NAME
-    }
-  end
+  let(:uplink_data_path) { 'spec/support/fixtures/integration/uplink_set_ethernet.json' }
 
   let(:log_int) { OneviewSDK::API300::C7000::LogicalInterconnect.get_all($client_300).first }
 
   describe '#create' do
     it 'can create the uplink and attach to a Logical Interconnect' do
-      item = klass.new($client_300, uplink_data)
+      item = klass.from_file($client_300, uplink_data_path)
       item[:logicalInterconnectUri] = log_int[:uri]
       expect { item.create }.not_to raise_error
       expect(item[:uri]).to be
--- spec/integration/resource/api300/c7000/uplink_set/update_spec.rb
+++ spec/integration/resource/api300/c7000/uplink_set/update_spec.rb
@@ -4,18 +4,22 @@ klass = OneviewSDK::API300::C7000::UplinkSet
 RSpec.describe klass, integration: true, type: UPDATE do
   include_context 'integration api300 context'
 
+  let(:uplink_data_path) { 'spec/support/fixtures/integration/uplink_set_ethernet.json' }
+  let(:interconnect) { OneviewSDK::API300::C7000::Interconnect.find_by($client_300, name: INTERCONNECT_2_NAME).first }
+  let(:enclosure) { OneviewSDK::API300::C7000::Enclosure.find_by($client_300, name: ENCL_NAME).first }
+  let(:network) { OneviewSDK::API300::C7000::EthernetNetwork.get_all($client_300).first }
+  let(:port) { interconnect['ports'].select { |item| item['portType'] == 'Uplink' && item['pairedPortName'] }.first }
+  subject(:uplink) { klass.from_file($client_300, uplink_data_path) }
+
+  before(:all) do
+    expect { uplink.create }.not_to raise_error # creating again because the #compliance test of logical_interconnect_update removes the uplink created before
+  end
+
+  before do
+    uplink.refresh
+  end
+
   describe '#update' do
-
-    subject(:uplink) { klass.find_by($client_300, name: UPLINK_SET4_NAME).first }
-    let(:interconnect) { OneviewSDK::API300::C7000::Interconnect.find_by($client_300, name: INTERCONNECT_2_NAME).first }
-    let(:enclosure) { OneviewSDK::API300::C7000::Enclosure.find_by($client_300, name: ENCL_NAME).first }
-    let(:network) { OneviewSDK::API300::C7000::EthernetNetwork.get_all($client_300).first }
-    let(:port) { interconnect['ports'].select { |item| item['portType'] == 'Uplink' && item['pairedPortName'] }.first }
-
-    before do
-      expect(uplink.retrieve!).to eq(true)
-    end
-
     it 'update port_config' do
       expect(uplink['portConfigInfos']).to be_empty
       uplink.add_port_config(
